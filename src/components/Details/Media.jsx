c6baf068c4f03350f5e19eb8a49c7248a5b41ff3
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2023-11-22 19:35:07 +0600

    working on Media dropdown

--- src/components/Details/Media.jsx
+++ src/components/Details/Media.jsx
@@ -4,13 +4,39 @@ import { Link } from "react-router-dom";
 export default function Media() {
     return (
         <div>
-            <ul className='absolute top-6 left-0 border shadow-md bg-black w-[200px] pl-7 py-5 rounded-md text-white overflow-auto space-y-2 text-base z-30'>
-                <li><Link to="">Backdrops</Link></li>
-                <li><Link to="">Logos</Link></li>
-                <li><Link to="">Posters</Link></li>
-                <li><Link className="flex" to="">
-                    <span>Videos</span>
-                    <RxTriangleRight /></Link></li>
+            <ul className='absolute top-6 left-0 border shadow-md bg-black w-[200px] py-5 px-7 rounded-md text-white overflow-auto space-y-2 text-base z-30'>
+                <li>
+                    <Link to="">
+                        <p className="flex items-center justify-between">
+                            <span className="text-white">Backdrops</span>
+                            <span>5</span>
+                        </p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="">
+                        <p className="flex items-center justify-between">
+                            <span className="text-white">Logos</span>
+                            <span>4</span>
+                        </p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="">
+                        <p className="flex items-center justify-between">
+                            <span className="text-white">Posters</span>
+                            <span>8</span>
+                        </p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="">
+                        <p className="flex items-center justify-between">
+                            <span className="text-white">Videos</span>
+                            <RxTriangleRight className="text-lg" />
+                        </p>
+                    </Link>
+                </li>
             </ul>
         </div>
     )
