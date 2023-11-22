cd605215da0b5b7328ff039285a784368a711457
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2023-11-22 19:03:49 +0600

    overview dropdown menu done

--- src/components/Details/DetailsHeader.jsx
+++ src/components/Details/DetailsHeader.jsx
@@ -1,14 +1,25 @@
+import { RxTriangleDown } from "react-icons/rx"
+import OverView from "./OverView";
+import "../style/DetailsHeader.css"
+import { useState } from "react";
 function DetailsHeader() {
+    const triangle = <RxTriangleDown className="text-xl cursor-pointer" />
+    const [openMenu, setOpenMenu] = useState(false);
+
     return (
         <div className="flex items-center justify-center mb-10">
             <div className="flex space-x-8">
-                <li className="border-b-4 pb-2">Overview</li>
-                <li className="border-b-4 pb-2">Media</li>
-                <li className="border-b-4 pb-2">Fandom</li>
-                <li className="border-b-4 pb-2">Share</li>
+                <li onMouseEnter={()=>setOpenMenu(true)} onMouseLeave={()=>setOpenMenu(false)} className="relative text-sm border-b-4 pb-2 flex items-start transition-all duration-500 border-sky-500">
+                     <span className="cursor-pointer">Overview</span>{triangle}
+                   {openMenu&& <OverView></OverView>}
+                </li>
+                <li className="text-sm border-b-4 pb-2 flex items-start">Media{triangle}</li>
+                <li className="text-sm border-b-4 pb-2 flex items-start">Fandom{triangle}</li>
+                <li className="text-sm border-b-4 pb-2 flex items-start">Share{triangle}</li>
             </div>
         </div>
     )
 }
 
 export default DetailsHeader;
+
