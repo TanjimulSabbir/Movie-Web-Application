566e92cc4b2ef914bf90bf97a5af78d4f56d3cc7
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-01-14 23:52:54 +0600

    RTK Setup

---
+++ src/Redux/Features/Api/apiSlice.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { baseAxios } from "../../../Tools/BaseAxios";
+
+export const apiSlice = createApi({
+    reducerPath: "api",
+    baseQuery: fetchBaseQuery({
+        baseUrl: baseAxios,
+        prepareHeaders: async (headers, { getState, endpoint }) => {
+            const token = getState().auth?.accessToken;
+            console.log(token, "from apiSlice")
+            if (token) {
+                headers.set("Authorization", `Bearer ${token}`)
+            }
+            return headers;
+        }
+    }),
+    tagTypes: [],
+    endpoints: (builder) => ({})
+});
