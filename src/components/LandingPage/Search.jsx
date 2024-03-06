a3e39cb37a13c1d5a23f662a225bc0afc7246401
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-03-06 16:08:40 +0600

    working on search input

--- src/components/LandingPage/Search.jsx
+++ src/components/LandingPage/Search.jsx
@@ -1,9 +1,10 @@
+import style from "../style/search.module.css"
 function Search() {
     return (
         <>
-            <div className="relative">
-                <input className="w-full rounded-3xl py-4 " type="text" placeholder="search for a movie, tv show, person..." />
-                <button className="absolute">Search</button>
+            <div className="relative mt-10">
+                <input className="w-full rounded-3xl py-3 border pl-5" type="text" placeholder="Search for a movie, tv show, person..." />
+                <button className={`${style.allSearchBtn}`}>Search</button>
             </div>
         </>
     )
